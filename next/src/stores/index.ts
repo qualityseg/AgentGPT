03c1a97bd4977508092c9ae78d0c9ee80afd2b9b
qualityseg <analytics.qualityseg@gmail.com>
2023-07-02 21:05:18 -0300

    vx

--- next/src/stores/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./messageStore";
-export * from "./agentStore";
-export * from "./modelSettingsStore";
-export * from "./themeStore";
--- setup.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal
-
-rem  The CLI will take care of setting up the ENV variables
-cd cli || exit /b 1
-call npm install
-npm run start
